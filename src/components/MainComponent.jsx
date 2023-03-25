e7b6e610d5f3243a665081d6975da5315abb3d15
clevermiraz <miraz.onesoft@gmail.com>
2023-03-25 16:43:20 +0600

    initial Commit

---
+++ src/components/MainComponent.jsx
@@ -0,0 +1,138 @@
+export default function MainComponent() {
+    return (
+        <div className="container">
+            <div className="one">
+                <h1 className="one">
+                    <span data-node-name="greeting">Hey</span>
+                    <span data-node-name="name">Lydia</span>
+                </h1>
+                <p className="two" data-node-name="greetingText">
+                    I really like your name btw!
+                </p>
+            </div>
+            <div className="three">
+                <p data-node-name="text1">It&apos;s your birthday!!! :D</p>
+            </div>
+            <div className="four">
+                <div className="text-box">
+                    <p className="hbd-chatbox" data-node-name="textInChatBox">
+                        Happy birthday to you!! Yeee! Many many happy blah...
+                    </p>
+                    <p className="fake-btn" data-node-name="sendButtonLabel">
+                        Send
+                    </p>
+                </div>
+            </div>
+            <div className="five">
+                <p className="idea-1" data-node-name="text2">
+                    That&apos;s what I was going to do.
+                </p>
+                <p className="idea-2" data-node-name="text3">
+                    But then I stopped.
+                </p>
+                <p className="idea-3">
+                    <span data-node-name="text4">I realised, I wanted to do something</span>
+                    <strong data-node-name="text4Adjective">special</strong>.
+                </p>
+                <p className="idea-4" data-node-name="text5Entry">
+                    Because,
+                </p>
+                <p className="idea-5">
+                    <span data-node-name="text5Content">You are Special</span>
+                    <span className="smiley" data-node-name="smiley">
+                        :)
+                    </span>
+                </p>
+                <p className="idea-6">
+                    <span data-node-name="bigTextPart1">S</span>
+                    <span data-node-name="bigTextPart2">O</span>
+                </p>
+            </div>
+            <div className="six">
+                <img src="img/lydia2.png" alt="" className="lydia-dp" data-node-name="imagePath" />
+                <img src="img/hat.svg" alt="" className="hat" />
+                <div className="wish">
+                    <h3 className="wish-hbd" data-node-name="wishHeading">
+                        Happy Birthday!
+                    </h3>
+                    <h5 data-node-name="wishText">May the js.prototypes always be with you! ;)</h5>
+                </div>
+            </div>
+            <div className="seven">
+                <div className="baloons">
+                    <img src="img/ballon2.svg" alt="" />
+                    <img src="img/ballon1.svg" alt="" />
+                    <img src="img/ballon3.svg" alt="" />
+                    <img src="img/ballon1.svg" alt="" />
+                    <img src="img/ballon2.svg" alt="" />
+                    <img src="img/ballon3.svg" alt="" />
+                    <img src="img/ballon2.svg" alt="" />
+                    <img src="img/ballon3.svg" alt="" />
+                    <img src="img/ballon1.svg" alt="" />
+                    <img src="img/ballon2.svg" alt="" />
+                    <img src="img/ballon3.svg" alt="" />
+                    <img src="img/ballon2.svg" alt="" />
+                    <img src="img/ballon1.svg" alt="" />
+                    <img src="img/ballon3.svg" alt="" />
+                    <img src="img/ballon2.svg" alt="" />
+                    <img src="img/ballon3.svg" alt="" />
+                    <img src="img/ballon1.svg" alt="" />
+                    <img src="img/ballon2.svg" alt="" />
+                    <img src="img/ballon1.svg" alt="" />
+                    <img src="img/ballon3.svg" alt="" />
+                    <img src="img/ballon3.svg" alt="" />
+                    <img src="img/ballon1.svg" alt="" />
+                    <img src="img/ballon2.svg" alt="" />
+                    <img src="img/ballon3.svg" alt="" />
+                    <img src="img/ballon2.svg" alt="" />
+                    <img src="img/ballon1.svg" alt="" />
+                    <img src="img/ballon3.svg" alt="" />
+                    <img src="img/ballon2.svg" alt="" />
+                    <img src="img/ballon3.svg" alt="" />
+                    <img src="img/ballon1.svg" alt="" />
+                    <img src="img/ballon2.svg" alt="" />
+                    <img src="img/ballon1.svg" alt="" />
+                    <img src="img/ballon3.svg" alt="" />
+                </div>
+            </div>
+            <div className="eight">
+                <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="20" cy="20" r="20" />
+                </svg>
+                <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="20" cy="20" r="20" />
+                </svg>
+                <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="20" cy="20" r="20" />
+                </svg>
+                <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="20" cy="20" r="20" />
+                </svg>
+                <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="20" cy="20" r="20" />
+                </svg>
+                <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="20" cy="20" r="20" />
+                </svg>
+                <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="20" cy="20" r="20" />
+                </svg>
+                <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="20" cy="20" r="20" />
+                </svg>
+                <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="20" cy="20" r="20" />
+                </svg>
+            </div>
+            <div className="nine">
+                <p data-node-name="outroText">Okay, now come back and tell me if you liked it.</p>
+                <p id="replay" data-node-name="replayText">
+                    Or click, if you want to watch it again.
+                </p>
+                <p className="last-smile" data-node-name="outroSmiley">
+                    :)
+                </p>
+            </div>
+        </div>
+    );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,27 @@
+import MainComponent from '@/components/MainComponent';
+import Head from 'next/head';
+import Script from 'next/script';
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Happy Birthday!!! Jannat:)</title>
+
+                <link
+                    href="https://fonts.googleapis.com/css?family=Work+Sans:300,400"
+                    rel="stylesheet"
+                />
+                <link rel="icon" href="favicon.ico" />
+            </Head>
+
+            <Script
+                src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/TweenMax.min.js"
+                strategy="lazyOnload"
+            />
+            <Script src="main.js" strategy="lazyOnload" />
+
+            <MainComponent />
+        </>
+    );
+}
